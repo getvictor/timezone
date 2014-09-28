32821209c37e65f430e738adf6b3b5b6434a9653
getvictor <victor.lyuboslavsky@gmail.com>
2014-09-28 12:56:47 -0500

    Updated 'start' to be compatible with OpenShift.

---
+++ server.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+var debug = require('debug')('timezone');
+var app = require('./app');
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP;
+
+app.set('port', server_port);
+
+var server = app.listen(server_port, server_ip_address, null, function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
